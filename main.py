878d0774e2952cb07d0a76a883add61cb02f8ced
carlos <calcobanunez@gmail.com>
2022-01-08 23:21:54 +0100

    Models generated

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-import pandas as pd
-import numpy as np
-from src.functions import *
-import matplotlib.pyplot as plt
-import seaborn as sns
-import kaplanmeier as km
 import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from lifelines import KaplanMeierFitter
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+
+from src.functions import *
+
 try:
     os.mkdir('images')
     print('Directory images created')
@@ -15,7 +23,7 @@ data_loaded = False
 while not data_loaded:
     working_data = input('Please enter the name of the file with the data: ')
     try:
-        data = pd.read_csv('data/'+working_data)
+        data = pd.read_csv('data/' + working_data)
         data_loaded = True
     except FileNotFoundError:
         print('Please enter a valid file name')
@@ -29,9 +37,8 @@ nan_column = ['AGE', 'TEMP', 'HEART_RATE', 'GLUCOSE', 'SAT_O2', 'BLOOD_PRES_SYS'
 data[nan_column] = data[nan_column].replace(0, np.nan)
 
 perc = 30.0
-min_count = int(((100-perc)/100)*data.shape[0] + 1)
+min_count = int(((100 - perc) / 100) * data.shape[0] + 1)
 data = data.dropna(axis=1, thresh=min_count)
-
 
 for col in data.columns:
     if data[col].isna().sum() <= 0.1 * data.shape[0]:
@@ -48,9 +55,106 @@ data = data[data.HEART_RATE < 250]
 data = data[data.SAT_O2 > 30]
 data.plot(kind='box', subplots=True, figsize=(20, 10))
 plt.savefig('images/box_plot')
+plt.close()
 
 data.hist(figsize=(15, 15), bins=20)
 plt.savefig('images/hist_plot')
+plt.close()
 
 sns.pairplot(data, hue='EXITUS')
 plt.savefig('images/pairplot_plot')
+plt.close()
+
+# Feature enconding
+
+# Extracting categorical and numerical columns
+
+cat_cols = [col for col in data.columns if data[col].dtype == 'object']
+num_cols = [col for col in data.columns if data[col].dtype != 'object']
+
+le = LabelEncoder()
+ohe = OneHotEncoder()
+
+for col in cat_cols:
+    if data[col].nunique() == 2:
+        data[col] = le.fit_transform(data[col])
+    else:
+        data[col] = ohe.fit_transform(data[col])
+
+# heatmap of data
+
+plt.figure(figsize=(12, 12))
+sns.heatmap(data.corr(), annot=True, linecolor='lightgrey', )
+plt.savefig('images/corr.png')
+plt.close()
+
+covid = get_num_people_by_age_category(data)
+
+kmf_covid = KaplanMeierFitter()
+kmf_covid.fit(data.DAYS_HOSPITAL[data['AGE_GROUP'] == "0-20"],
+              data['EXITUS'][data['AGE_GROUP'] == "0-20"], label='0-20')
+ax = kmf_covid.plot_survival_function()
+kmf_covid.fit(data.DAYS_HOSPITAL[data['AGE_GROUP'] == "21-40"],
+              data['EXITUS'][data['AGE_GROUP'] == "21-40"], label='21-40')
+ax = kmf_covid.plot_survival_function(ax=ax)
+kmf_covid.fit(data.DAYS_HOSPITAL[data['AGE_GROUP'] == "41-60"],
+              data['EXITUS'][data['AGE_GROUP'] == "41-60"], label='41-60')
+ax = kmf_covid.plot_survival_function(ax=ax)
+kmf_covid.fit(data.DAYS_HOSPITAL[data['AGE_GROUP'] == "61-80"],
+              data['EXITUS'][data['AGE_GROUP'] == "61-80"], label='61-80')
+ax = kmf_covid.plot_survival_function(ax=ax)
+kmf_covid.fit(data.DAYS_HOSPITAL[data['AGE_GROUP'] == "81-100"],
+              data['EXITUS'][data['AGE_GROUP'] == "81-100"], label='81-100')
+ax = kmf_covid.plot_survival_function(ax=ax)
+kmf_covid.fit(data.DAYS_HOSPITAL[data['AGE_GROUP'] == "101-120"],
+              data['EXITUS'][data['AGE_GROUP'] == "101-120"], label='101-120')
+ax = kmf_covid.plot_survival_function(ax=ax)
+ax.get_figure().savefig("images/km_graph.png")
+
+# Generating a model
+
+y = covid.EXITUS
+x = covid.drop(columns=['EXITUS', 'AGE_GROUP'], axis=1)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+
+best_features_Chi2 = SelectKBest(score_func=chi2, k=7)
+fit_Chi2 = best_features_Chi2.fit(x_train, y_train)
+
+# gráfico de barras utilizando matplotlib
+df = pd.DataFrame({'Nombre_feat': x.columns, 'values': fit_Chi2.scores_})
+plt.figure(figsize=(12, 12))
+ax = df.plot.barh(x='Nombre_feat', y='values', rot=0)
+plt.savefig('images/chicuadrado.jpg')
+plt.close()
+
+# Normalizing data
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(x_train)
+X_test = scaler.fit_transform(x_test)
+
+dt_model = generate_model(x_train, y_train, x_test, y_test, DecisionTreeClassifier(random_state=4))
+
+print(f"Training Accuracy of Decision Tree is {dt_model[1]}")
+print(f"Test Accuracy of Decision Tree is {dt_model[2].round(2)} \n")
+
+print(f"Confusion Matrix :- \n{dt_model[3]}\n")
+print(f"Classification Report :- \n {dt_model[4]}")
+
+knn_model = generate_model(X_train, y_train, X_test, y_test, KNeighborsClassifier())
+
+print(f"Training Accuracy of KNeighbors is {knn_model[1]}")
+print(f"Test Accuracy of KNeighbors is {knn_model[2].round(2)} \n")
+
+print(f"Confusion Matrix :- \n{knn_model[3]}\n")
+print(f"Classification Report :- \n {knn_model[4]}")
+
+mlp_model = generate_model(X_train, y_train, X_test, y_test, MLPClassifier(max_iter=1000, random_state=0))
+
+print(f"Training Accuracy of KNeighbors is {mlp_model[1]}")
+print(f"Test Accuracy of KNeighbors is {mlp_model[2].round(2)} \n")
+
+print(f"Confusion Matrix :- \n{mlp_model[3]}\n")
+print(f"Classification Report :- \n {mlp_model[4]}")
--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,5 @@
-
+import pandas as pd
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 def random_value_imputation(database, feature):
     random_sample = database[feature].dropna().sample(database[feature].isna().sum())
@@ -11,3 +12,20 @@ def impute_mode(database, feature):
     mode = database[feature].mode()[0]
     database[feature] = database[feature].fillna(mode)
     return database
+
+
+def get_num_people_by_age_category(df):
+    df["AGE_GROUP"] = pd.cut(x=df['AGE'], bins=[0, 20, 40, 60, 80, 100, 120],
+                             labels=["0-20", "21-40", "41-60", '61-80', '81-100', '101-120'])
+    return df
+
+
+def generate_model(x_train, y_train, x_test, y_test, model):
+    model.fit(x_train, y_train)
+    y_pred_train = model.predict(x_train)
+    y_pred = model.predict(x_test)
+    acc_train = accuracy_score(y_train, y_pred_train)
+    acc_test = accuracy_score(y_test, y_pred)
+    confusion_matrix_model = confusion_matrix(y_test, y_pred)
+    classification_report_model = classification_report(y_test, y_pred)
+    return model, acc_train, acc_test, confusion_matrix_model, classification_report_model
